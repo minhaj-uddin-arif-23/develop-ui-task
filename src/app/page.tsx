f342db24ad41e61702541f693abbc010328be57b
unknown <arifk62901@gmail.com>
2025-06-29 17:16:51 +0600

    work process section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroSection />
       <Skills />
       <Development />
-      {/* <WorkProcess /> */}
+      <WorkProcess />
       {/* <ContactSection /> */}
     </div>
   );
